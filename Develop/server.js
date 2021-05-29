8a139f8f88381987bac8b63af0f0568590a3c7a8
Danrub98 <jrm.daniel02@gmail.com>
2021-05-29 14:16:13 -0500

    Second commit

--- Develop/server.js
+++ Develop/server.js
@@ -3,16 +3,16 @@ const path = require ('path');
  
 
 const app = express();
-const PORT = 8080;
+var PORT = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
 
-//app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
-//app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/add', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 
 require('./routes/apiRoute')(app);
 require('./routes/htmlRoute')(app);
